e55d9c2f0350720f3b83a199c8285a6b3642a130
AlfarizaRasendria <rasendria.alfariza18@gmail.com>
2024-05-24 23:44:04 +0700

    Initializing Repository (FE,BE, and all of the configuration files to deploy to the google cloud platform

---
+++ MLGC-Submission-Alfariza/src/server/handler.js
@@ -0,0 +1,64 @@
+const predictClassification = require("../services/inferenceService");
+const crypto = require("crypto");
+const storeData = require("../services/storeData");
+const { Firestore } = require('@google-cloud/firestore');
+
+async function postPredictHandler(request, h) {
+  const { image } = request.payload;
+  const { model } = request.server.app;
+
+  const { label, suggestion } = await predictClassification(model, image);
+  const id = crypto.randomUUID();
+  const createdAt = new Date().toISOString();
+
+  const data = {
+    id: id,
+    result: label,
+    suggestion: suggestion,
+    createdAt: createdAt,
+  };
+
+  await storeData(id, data);
+
+  const response = h
+    .response({
+      status: "success",
+      message: "Model is predicted successfully",
+      data: data,
+    })
+    .code(201);
+
+  return response;
+}
+
+async function getPredictHistoriesHandler(request, h) {
+  let histories = [];
+  
+  const db = new Firestore({
+    databaseId: "cancer-ml-db",
+  });
+  const predictionCollection = db.collection("predictions");
+  const predictionSnapshots = await predictionCollection.get();
+
+  predictionSnapshots.forEach((document) => {
+    const documentId = document.id;
+    const documentData = document.data();
+
+    const historyData = {
+      id: documentId,
+      history: documentData,
+    };
+    histories.push(historyData);
+  });
+
+  const response = h
+    .response({
+      status: "success",
+      data: histories,
+    })
+    .code(200);
+
+  return response;
+}
+
+module.exports = { postPredictHandler, getPredictHistoriesHandler };
